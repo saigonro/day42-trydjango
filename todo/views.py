105d8e01baf0db1c4315a6f5cb1d99312d6a6ba1
Dan Dumitrache <vietnamezul@gmail.com>
2018-03-07 14:29:24 +0000

    add templating

--- todo/views.py
+++ todo/views.py
@@ -3,36 +3,46 @@ from django.http import HttpResponse
 from .models import TodoItem
 from .forms import TodoItemForm, EditTodoItemForm
 
-def get_todo_page(request):
+# Create your views here.
+def get_index(request):
     if request.method == "POST":
+        print("It's the POST")        
         form = TodoItemForm(request.POST)
         if form.is_valid():
             form.save()
     else:
-        form = TodoItemForm()
-    
+        print("It's the GET")        
+
+    form = TodoItemForm()
     items = TodoItem.objects.all()
-    return render(request, "todo/todo_items.html", {'items': items, 'form': form})
-    
-def delete_todo_item(request, id):
+    return render(request, "todo/index.html", {'items': items, 'form': form})
+
+
+def edit_todo_item(request, id): 
     item = get_object_or_404(TodoItem, pk=id)
-    item.delete()
-    return redirect("/")
-    
-def toggle_todo_item(request, id):
-    item = get_object_or_404(TodoItem, pk=id)
-    item.done = not item.done
-    item.save()
-    return redirect("/")
-    
-def edit_todo_item(request, id):
-    item = get_object_or_404(TodoItem, pk=id)
+
     if request.method == "POST":
+        print("It's the POST")
         form = EditTodoItemForm(request.POST, instance=item)
         if form.is_valid():
             form.save()
-            return redirect("/")
+            return redirect("todo_index")
     else:
-        print("It's a GET")
+        print("It's the GET")
+        
     form = EditTodoItemForm(instance=item)
     return render(request, "todo/todo_item_form.html", {'form': form})
+
+
+
+def delete_todo_item(request, id):
+    item = get_object_or_404(TodoItem, pk=id)
+    item.delete()
+    return redirect("todo_index")
+    
+def toggle_status(request, id):
+    print(request.method)
+    item = get_object_or_404(TodoItem, pk=id)
+    item.done = not item.done
+    item.save()
+    return redirect("todo_index")
--- trydjango/urls.py
+++ trydjango/urls.py
@@ -1,26 +1,14 @@
-"""trydjango URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
-from todo.views import get_todo_page, delete_todo_item, toggle_todo_item, edit_todo_item
+
+from todo import urls as todo_urls
+from comments import urls as comments_urls
+
+import home.views as home_views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', get_todo_page),
-    url(r'^delete/(\d+)$', delete_todo_item),
-    url(r'^toggle/(\d+)$', toggle_todo_item),
-    url(r'^edit/(\d+)$', edit_todo_item),
+    url(r'^$', home_views.get_index, name="home"),
+    url(r'^chat/', include(comments_urls)),
+    url(r'^todo/', include(todo_urls)),
 ]
